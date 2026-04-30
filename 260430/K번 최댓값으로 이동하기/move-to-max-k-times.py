ab8460136b0bbd026658e7a6f9578a5082125a2b
제다빈 <notbad1700@gmail.com>
2026-04-30 10:31:36 +0900

    Title: K번 최댓값으로 이동하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260430/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 260430/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -5,18 +5,19 @@ dxy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 n, k = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 r, c = map(int, input().split())
+r, c = r - 1, c - 1
 
 def rxcy(rc):
     for i in range(n):
         for j in range(n):
             if grid[i][j] == rc:
-                return i + 1, j + 1
+                return i, j
 
 # Please write your code here.
 for _ in range(k):
     visited = [[False] * n for _ in range(n)]
-    visited[r - 1][c - 1] = True
-    deq = deque([(r - 1, c - 1)])
+    visited[r][c] = True
+    deq = deque([(r, c)])
 
     while deq:
         x, y = deq.popleft()
@@ -24,7 +25,7 @@ for _ in range(k):
         for dx, dy in dxy:
             nx, ny = x + dx, y + dy
 
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and grid[nx][ny] < grid[r - 1][c - 1]:
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and grid[nx][ny] < grid[r][c]:
                 visited[nx][ny] = True
                 deq.append((nx, ny))
 
@@ -32,11 +33,12 @@ for _ in range(k):
     for i in range(n):
         for j in range(n):
             if visited[i][j]:
-                if rc < grid[i][j] < grid[r - 1][c - 1]:
+                if rc < grid[i][j] < grid[r][c]:
                     rc = grid[i][j]
+                    
     if rc == 0:
         break
     else:
         r, c = rxcy(rc)
 
-print(r, c)
+print(r + 1, c + 1)
