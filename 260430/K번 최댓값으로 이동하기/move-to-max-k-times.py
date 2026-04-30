504564925268fc38ac06e567b8ce1ccdab37b66f
제다빈 <notbad1700@gmail.com>
2026-04-30 10:50:10 +0900

    Title: K번 최댓값으로 이동하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260430/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 260430/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -2,21 +2,22 @@ from collections import deque
 
 dxy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-n, k = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
+def target(next_num):
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == next_num:
+                return i, j
+
+N, K = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
 r, c = map(int, input().split())
 r, c = r - 1, c - 1
 
-def rxcy(rc):
-    for i in range(n):
-        for j in range(n):
-            if grid[i][j] == rc:
-                return i, j
-
-# Please write your code here.
-for _ in range(k):
-    visited = [[False] * n for _ in range(n)]
+for _ in range(K):
+    visited = [[False] * N for _ in range(N)]
     visited[r][c] = True
+    cur_num = board[r][c]
+    next_num = 0
     deq = deque([(r, c)])
 
     while deq:
@@ -25,20 +26,15 @@ for _ in range(k):
         for dx, dy in dxy:
             nx, ny = x + dx, y + dy
 
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and grid[nx][ny] < grid[r][c]:
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and board[nx][ny] < cur_num:
                 visited[nx][ny] = True
+                if board[nx][ny] > next_num:
+                    next_num = board[nx][ny]
                 deq.append((nx, ny))
 
-    rc = 0
-    for i in range(n):
-        for j in range(n):
-            if visited[i][j]:
-                if rc < grid[i][j] < grid[r][c]:
-                    rc = grid[i][j]
-                    
-    if rc == 0:
-        break
+    if next_num:
+        r, c = target(next_num)
     else:
-        r, c = rxcy(rc)
+        r, c = r, c
 
 print(r + 1, c + 1)
