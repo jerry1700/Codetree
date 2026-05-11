a2149c5cc9d4bc09f0a41a49fcaa2cb1d5c49169
제다빈 <notbad1700@gmail.com>
2026-05-11 15:34:42 +0900

    [Passed] 4가지 연산을 이용하여 1 만들기 (78ms, 25MB)

--- trail4/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ trail4/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -4,7 +4,7 @@ N = int(input())
 
 dist = [-1] * 1000001
 dist[N] = 0
-q = deque([(N)])
+q = deque([N])
 
 while q:
     x = q.popleft()
@@ -12,25 +12,16 @@ while q:
     if x == 1:
         break
 
-    if x % 2 == 0 and x % 3 == 0:
-        for nx in [x - 1, x + 1, x // 2, x // 3]:
-            if 0 <= nx < 1000001 and dist[nx] == -1:
-                dist[nx] = dist[x] + 1
-                q.append((nx))
-    elif x % 2 == 0:
-        for nx in [x - 1, x + 1, x // 2]:
-            if 0 <= nx < 1000001 and dist[nx] == -1:
-                dist[nx] = dist[x] + 1
-                q.append((nx))
-    elif x % 3 == 0:
-        for nx in [x - 1, x + 1, x // 3]:
-            if 0 <= nx < 1000001 and dist[nx] == -1:
-                dist[nx] = dist[x] + 1
-                q.append((nx))
-    else:
-        for nx in [x - 1, x + 1]:
-            if 0 <= nx < 1000001 and dist[nx] == -1:
-                dist[nx] = dist[x] + 1
-                q.append((nx))
+    dx = [x - 1, x + 1]
+
+    if x % 2 == 0:
+        dx.append(x // 2)
+    if x % 3 == 0:
+        dx.append(x // 3)
+
+    for nx in dx:
+        if 0 <= nx < 1000001 and dist[nx] == -1:
+            dist[nx] = dist[x] + 1
+            q.append(nx)
 
 print(dist[1])
