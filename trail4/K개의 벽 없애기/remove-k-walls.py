c43c8d246a010f31f394235eda565a56b38f5299
제다빈 <notbad1700@gmail.com>
2026-05-11 15:14:57 +0900

    [Passed] K개의 벽 없애기 (60ms, 17MB)

--- trail4/K개의 벽 없애기/remove-k-walls.py
+++ trail4/K개의 벽 없애기/remove-k-walls.py
@@ -1,17 +1,14 @@
 from collections import deque
+from itertools import combinations
 
 dxy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 N, K = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-temp = [row[:] for row in board]
-start = tuple(map(int, input().split()))
-start = (start[0] - 1, start[1] - 1)
-end = tuple(map(int, input().split()))
-end = (end[0] - 1, end[1] - 1)
+start = tuple(x - 1 for x in map(int, input().split()))
+end = tuple(x - 1 for x in map(int, input().split()))
 
 walls = [(i, j) for i in range(N) for j in range(N) if board[i][j] == 1]
-length = len(walls)
 
 def bfs():
     dist = [[-1] * N for _ in range(N)]
@@ -19,7 +16,10 @@ def bfs():
     q = deque([start])
     
     while q:
-        x, y = q.popleft()
+        x, y = q.popleft() 
+
+        if (x, y) == end:
+            return dist[x][y]
 
         for dx, dy in dxy:
             nx, ny = x + dx, y + dy
@@ -28,34 +28,22 @@ def bfs():
                 dist[nx][ny] = dist[x][y] + 1
                 q.append((nx, ny))
 
-    return dist[end[0]][end[1]]
-
-def backtracking(start, depth):
-    global result
-    global board
-
-    if depth == K:
-        for se in sequence:
-            board[walls[se][0]][walls[se][1]] = 0
-
-        answer = bfs()
-        if 0 < answer < result:
-            result = answer
-
-        board = [row[:] for row in temp]
-
-        return
-    
-    for i in range(start, length):
-        sequence.append(i)
-        backtracking(i + 1, depth + 1)
-        sequence.pop()
+    return -1
 
 result = float("inf")
-sequence = []
-backtracking(0, 0)
+
+for wall in combinations(walls, K):
+    for r, c in wall:
+        board[r][c] = 0
+
+    answer = bfs()
+    if 0 < answer < result:
+        result = answer
+
+    for r, c in wall:
+        board[r][c] = 1
 
 if result == float("inf"):
-    print(-1)
-else:
-    print(result)
+    result = -1
+
+print(result)
