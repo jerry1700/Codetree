3b64d1baf637b90f27d873c5a8730e70594c3347
제다빈 <notbad1700@gmail.com>
2026-05-13 10:21:20 +0900

    [Passed] 서로 다른 BST 개수 세기 (45ms, 16MB)

--- trail4/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ trail4/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -3,8 +3,7 @@ N = int(input())
 dp = [0] * 20
 dp[0] = 1
 dp[1] = 1
-dp[2] = 2
-for i in range(3, N + 1):
+for i in range(2, N + 1):
     dp[i] = sum(dp[num] * dp[i - 1 - num] for num in range(i))
 
 print(dp[N])
